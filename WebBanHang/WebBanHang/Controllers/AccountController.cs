1daef93ce42ebe648072a090646b7df95fd6f974
Kouemi <minhlam238@gmail.com>
2019-11-12 21:23:35 +0700

    Thêm mục upload file và download file cho người dùng

--- WebBanHang/WebBanHang/Controllers/AccountController.cs
+++ WebBanHang/WebBanHang/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -45,12 +46,44 @@ namespace WebBanHang.Controllers
       
         [HttpPost]
         public string DangKy(string last_name, string first_name, string email, string password,
-            string avatar, string job_title, string department, int manager_id, string phone,
+            HttpPostedFileBase avatar, string job_title, string department, int manager_id, string phone,
             string address1, string address2, string city, string state, string postal_code,
             string country)
         {
                 try
             {
+                string _FileName = "";
+                string datetimeFolderName = "";
+                //Di chuyen file vao thu muc mong muon
+                if(avatar.ContentLength > 0 )
+                {
+                     _FileName = Path.GetFileName(avatar.FileName);
+                    string _FileNameExtension = Path.GetExtension(avatar.FileName);
+                    if((_FileNameExtension == ".png"
+                        || _FileNameExtension == ".jpg"
+                        || _FileNameExtension == ".jpeg"
+                        || _FileNameExtension == ".docx"
+                        || _FileNameExtension == ".xls"
+                        || _FileNameExtension == ".xlsx") == false)
+                    {
+                        return String.Format("File có đuôi {0} không hợp lệ. Vui lòng kiểm tra lại", _FileNameExtension);
+                    }
+
+                    DateTime now = DateTime.Now;
+                    datetimeFolderName = String.Format("{0}{1}{2}{3}{4}", now.Year, now.Month, now.Day, now.Hour, now.Minute); //201911122018
+                    string uploadedFolderPath = Server.MapPath("~/UploadedFiles"); //UploadedFiles/201911122018/ten file uploaed
+                    if (Directory.Exists(uploadedFolderPath) == false)// Nếu thư mục cần lưu trữ file upload không tồn tại ( chưa có ) => tạo mới
+                    {
+                        Directory.CreateDirectory(uploadedFolderPath);
+                    }
+
+                    string _path = Path.Combine(uploadedFolderPath, _FileName);
+                    avatar.SaveAs(_path);
+                }
+
+
+
+
                 using (QuanLyBanHangEntities context = new QuanLyBanHangEntities())
                 {
                     employees newRow = new employees();
@@ -58,7 +91,7 @@ namespace WebBanHang.Controllers
                     newRow.first_name = first_name;
                     newRow.email = email;
                     newRow.password = password;
-                    newRow.avatar = avatar;
+                    newRow.avatar = datetimeFolderName + "/" + _FileName;
                     newRow.job_title = job_title;
                     newRow.department = department;
                     newRow.manager_id = manager_id;
