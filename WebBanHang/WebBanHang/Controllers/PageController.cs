9a59f20b58331b7c11561aa8bf87389b20a1a44b
Kouemi <minhlam238@gmail.com>
2019-11-14 23:20:19 +0700

    Thêm trang sản phẩm

--- WebBanHang/WebBanHang/Controllers/PageController.cs
+++ WebBanHang/WebBanHang/Controllers/PageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanHang.EFF;
 
 namespace WebBanHang.Controllers
 {
@@ -10,6 +11,16 @@ namespace WebBanHang.Controllers
     {
         public ActionResult Index()
         {
+            List<products> lstProduct = new List<products>();
+
+            using (QuanLyBanHangEntities context = new QuanLyBanHangEntities()) 
+            {
+                lstProduct = context.products.ToList();
+            }
+            // Truyền dữ liệu từ controller => View thông qua viewBag
+            //Tên View mới DanhSachSanPham
+            ViewBag.DanhSachSanPham = lstProduct;
+
             return View();
         }
         
